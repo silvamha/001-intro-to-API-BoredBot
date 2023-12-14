dd465dc46e875455dc9cec255abab21cc77cd510
silvamha <silva.email@yahoo.com>
2023-12-13 19:54:30 -0500

    Updated application

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 // Initialize Cloud Firestore through Firebase
 import { initializeApp } from "firebase/app";
 
-import {
-  getAuth,
-  onAuthStateChanged,  
-} from "firebase/auth";
-
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 import {
   getFirestore,
   collection,
   getDocs,
-  addDoc, 
+  addDoc,
   onSnapshot,
   doc,
   setDoc,
@@ -53,7 +49,11 @@ async function addCityWithId(cityId, city) {
   }
 }
 
-addCityWithId("newCityId", { name: "Flordiablanca", population: 12350, country: "Country" });
+addCityWithId("newCityId", {
+  name: "Flordiablanca",
+  population: 12350,
+  country: "Country",
+});
 
 function listenForCityUpdates(cityId) {
   const cityRef = doc(db, "cities", cityId);
@@ -71,14 +71,74 @@ function listenForCityUpdates(cityId) {
 
 listenForCityUpdates("newCityId");
 
-function iterateObject(obj){
-  for (let key in obj){
-    console.log(`Key:, ${key}, Value:, ${obj[key]}`)
+// ***** Study and understand these concepts*******************
+
+function iterateObject(obj) {
+  for (let key in obj) {
+    console.log(`Key:, ${key}, Value:, ${obj[key]}`);
   }
 }
 
-iterateObject(db)
+const studentObj = [
+  {
+    name: "John",
+    age: 16,
+    scores: {
+      maths: 74,
+      english: 63,
+      science: 85,
+    },
+  },
+
+  {
+    name: "Mark",
+    age: 21,
+    scores: {
+      math: 84,
+      english: 69,
+      science: 95,
+    },
+  },
+];
 
 
-console.log('I want to live in Savannah')
-console.log('Changed API key')
+
+const person = { name: 'Alice', age: 25 };
+for (let key in person) {
+    console.log(key); // Outputs: 'name', 'age'
+}
+
+
+const numbers = [10, 20, 30];
+for (let number of numbers) {
+    console.log(number); // Outputs: 10, 20, 30
+}
+
+
+
+
+
+const data = [
+  { 
+      name: "Irene",
+      favoriteColor: "blue", 
+      maritalStatus: "looking",
+      details: { 
+          age: 29, 
+          location: "Taiwan" 
+      }
+  },
+  { 
+      name: "Monica", 
+      favoriteColor: "red",
+      maritalStatus: "married",
+      details: { 
+          age: 45, 
+          location: "Colombia" 
+      }
+  }
+];
+
+for (let key in obj) {
+  console.log(key);
+}
